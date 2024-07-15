9e831d7296b7aae2a2413ddeb43934627d7dd9d2
Sapphirenatalie <168541642+Sapphirenatalie@users.noreply.github.com>
2024-07-15 22:58:47 +0300

    Update module_3_1_old.py

--- module_3_1_old.py
+++ module_3_1_old.py
@@ -14,20 +14,3 @@ def test_2(a, b, c):
 
 
 test_2(4, 34, 78)
-
-d = 1
-
-print('----------')
-
-
-def test_function():
-    d = 10
-
-    def inner_function():
-        d = 'local'
-        print("Я в области видимости функции test_function")
-
-    inner_function()
-
-
-test_function()
