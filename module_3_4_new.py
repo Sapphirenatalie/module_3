57ca10242f9ceeb5b680a3153dd4a6f2117243c2
Sapphirenatalie <168541642+Sapphirenatalie@users.noreply.github.com>
2024-07-19 08:09:53 +0300

    Update module_3_4_new.py

--- module_3_4_new.py
+++ module_3_4_new.py
@@ -32,9 +32,7 @@ def single_root_words(root_word, *other_words):
     same_words = []
     for word in other_words:
         if root_word.lower() in word.lower() or word.lower() in root_word.lower():
-            same_words.append(word)
-        else:
-            continue
+            same_words.append(word)        
     return same_words
 
 
